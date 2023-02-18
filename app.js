245a354a1c707e62beb5332c9f426383ef24dbda
baarayy <baraaahmed728@gmail.com>
2023-02-18 11:07:43 +0200

    started js

---
+++ app.js
@@ -0,0 +1,4 @@
+let choices = document.querySelectorAll('.num');
+let submit = document.getElementById('btn');
+let evPage = document.querySelector('evaluation');
+let retPage = document.querySelector('thanks');
