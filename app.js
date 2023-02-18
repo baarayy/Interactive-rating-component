986952756ab35f900ab36985344344414d59e09c
baarayy <baraaahmed728@gmail.com>
2023-02-18 11:16:07 +0200

    Added Selected class to css

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ let choices = document.querySelectorAll('.num');
 let submit = document.getElementById('btn');
 let evPage = document.querySelector('evaluation');
 let retPage = document.querySelector('thanks');
+let reslt = document.getElementById('rate');
