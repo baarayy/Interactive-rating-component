71fc221394fdc80d00002ebf51f725f0a244f500
baarayy <baraaahmed728@gmail.com>
2023-02-18 11:26:27 +0200

    Finished

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let choices = document.querySelectorAll('.num');
 let submit = document.getElementById('btn');
-let evPage = document.querySelector('evaluation');
-let retPage = document.querySelector('thanks');
+let evPage = document.querySelector('.evaluation');
+let retPage = document.querySelector('.thanks');
 let reslt = document.getElementById('rate');
+for(let i = 0 ; i < choices.length ; i++) {
+    choices[i].addEventListener('click' , ()=>{
+        for(let i = 0 ; i < choices.length ; i++) {
+            choices[i].classList.remove('selected');
+        }
+        choices[i].classList.add('selected');
+        submit.addEventListener('click' , ()=> {
+            evPage.style.display = 'none';
+            retPage.style.display = 'block';
+            reslt.innerHTML = choices[i].innerHTML;
+        })
+    })
+}
